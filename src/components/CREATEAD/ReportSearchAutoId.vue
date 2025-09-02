e253c05466e3353b71437bf968bac7cdbb1549fa
bigboykirusha <105214100+bigboykirusha@users.noreply.github.com>
2025-09-02 15:07:10 +0300

    fix

--- src/components/CREATEAD/ReportSearchAutoId.vue
+++ src/components/CREATEAD/ReportSearchAutoId.vue
@@ -60,7 +60,7 @@
                </div>
                <div class="report-search__field">
                   <span class="report-search__label">Пробег:</span>
-                  <span class="report-search__value">{{ formattedResult.mileage }} км</span>
+                  <span class="report-search__value">{{ formattedResult.mileage }} </span>
                </div>
                <div class="report-search__field">
                   <span class="report-search__label">Ссылка:</span>
@@ -98,8 +98,15 @@ const validateVIN = (vin) => {
    return true;
 };
 
-const validRegions = new Set([/* список регионов */]);
-
+const validRegions = new Set([
+   "01", "02", "102", "702", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "113", "14", "15", "16", "116", "716",
+   "17", "18", "19", "21", "121", "22", "23", "93", "123", "193", "24", "84", "88", "124", "25", "125", "26", "126", "27", "28", "29",
+   "30", "31", "32", "33", "34", "134", "35", "36", "136", "37", "38", "85", "138", "39", "91", "40", "41", "42", "142", "43", "44",
+   "45", "46", "47", "147", "48", "49", "50", "90", "150", "190", "750", "790", "51", "52", "152", "53", "54", "154", "55", "56",
+   "57", "58", "59", "81", "159", "60", "61", "161", "761", "62", "63", "163", "763", "64", "164", "65", "66", "96", "196", "67", "68",
+   "69", "70", "71", "72", "73", "173", "174", "774", "75", "80", "76", "77", "97", "99", "177", "197", "199", "777", "799", "797",
+   "78", "98", "178", "198", "79", "82", "83", "86", "186", "87", "89"
+]);
 const isValidLicensePlate = (value) => {
    const regex =
       /^[АВЕКМНОРСТУХABEKMHOPCTYX](?!000)\d{3}[АВЕКМНОРСТУХABEKMHOPCTYX]{2}(\d{2,3})$/u;
@@ -129,8 +136,12 @@ const formattedResult = computed(() => {
          year: ad.auto_technical_specifications?.[0]?.year_release?.title || "—",
          state: ad.auto_history_conditions?.[0]?.state?.title || "—",
          owners: ad.auto_history_conditions?.[0]?.count_owners?.title || "—",
-         mileage: ad.auto_history_conditions?.[0]?.mileage || "—",
-         price: ad.ads_parameter?.amount ? `${ad.ads_parameter.amount} ₽` : "—",
+         mileage: ad.auto_history_conditions?.[0]?.mileage
+            ? `${ad.auto_history_conditions[0].mileage} км`
+            : "—",
+         price: ad.ads_parameter?.amount
+            ? `${ad.ads_parameter.amount} ₽`
+            : "—",
          color: ad.auto_appearances?.[0]?.color?.[0]?.title || "—",
          url: ad.url,
       };
